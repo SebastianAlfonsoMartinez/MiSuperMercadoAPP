bbfacfef48ecc0b854c2b6f844d3778654330929
SebastianAlfonsoMartinez <sebastianalfonso94@gmail.com>
2023-09-24 14:07:15 -0500

    Definiendo variables para almacenar datos del los productos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String productName = "Leche Entera Alpina";
+        String description = "Bolsa x 1.100 ml";
+        String category = "Lacteo/Huevos y refrigerados";
+        String label = "Leches";
+        double price = 3100;
+        String urlPhoto = "https://www.eurosupermercados.com/eurosupermercado2020//contenidos/images/ecommerce_productos/7702001041404.jpg";
+
+        System.out.println("Informacion del producto: \n" + "Nombre del producto: " + productName + "\nDescripcion: "
+                + description + "\nCategoria: " + category + "\nEtiqueta: " + label + "\nPrecio: $" + price + "Url Foto: " + urlPhoto);
+
+
     }
+
 }
